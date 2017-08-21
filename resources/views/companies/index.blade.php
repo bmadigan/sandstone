1e8ec0907590777640ad7c64eb922fa9f47891f0
Brad Madigan <bradmadigan@gmail.com>
2017-08-21 18:51:06 -0400

    Put the Index table into a Card

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -14,49 +14,54 @@
     <div class="container m-xs-b-4">
         <div class="m-xs-b-6">
 
-            <div class="row">
-
-                <div class="alert alert-notify ">
-                    * This table is a standard Eloquent pagination example with a Blade directive
-                </div>
-
-                <table class="table" cellpadding="0" cellspacing="0">
-                    <thead>
-                    <tr>
-                        <th class="pl2">Company</th>
-                        <th>Address</th>
-                        <th>City</th>
-                        <th>Contact</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($companies as $company)
-                            <tr>
-                                <td class="ph2">
-                                    <a href="{{ $company->path() }}">
-                                        {{ $company->company_name }}
-                                    </a>
-                                </td>
-                                <td>
-                                    {!! $company->displayStreetAddress() !!}
-                                </td>
-                                <td>{{ $company->city }}</td>
-                                <td>{!! $company->displayContact() !!}</td>
-                                <td>
-                                    <a href="{{ route('companies.edit', $company) }}">
-                                        <i class="fa fa-pencil-square m-xs-2" aria-hidden="true"></i>
-                                    </a>
-                                    <a href=""><i class="fa fa-trash" aria-hidden="true"></i></a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-
-                {{ $companies->links() }}
-
+            <div class="alert alert-notify ">
+                * This table is a standard Eloquent pagination example with a Blade directive
             </div>
-        </div>
+
+            <div class="row">
+                <div class="card">
+                    <div class="card-section">
+
+                        <table class="table" cellpadding="0" cellspacing="0">
+                            <thead>
+                            <tr>
+                                <th>Company</th>
+                                <th>Address</th>
+                                <th>City</th>
+                                <th>Contact</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($companies as $company)
+                                    <tr>
+                                        <td>
+                                            <a href="{{ $company->path() }}">
+                                                {{ $company->company_name }}
+                                            </a>
+                                        </td>
+                                        <td>
+                                            {!! $company->displayStreetAddress() !!}
+                                        </td>
+                                        <td>{{ $company->city }}</td>
+                                        <td>{!! $company->displayContact() !!}</td>
+                                        <td>
+                                            <a href="{{ route('companies.edit', $company) }}">
+                                                <i class="fa fa-pencil-square m-xs-2" aria-hidden="true"></i>
+                                            </a>
+                                            <a href=""><i class="fa fa-trash" aria-hidden="true"></i></a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                        {{ $companies->links() }}
+
+                    </div><!--/card-section-->
+                </div><!--/card-->
+
+            </div><!--/row-->
+        </div><!--/m-xs-->
     </div>
 </div>
 
