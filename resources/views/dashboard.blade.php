b404eeff35dd975eaa91b064f9d6efe9b81f3811
Brad Madigan <bradmadigan@gmail.com>
2017-08-22 11:48:32 -0400

    Wrap the view into a card section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,15 +16,18 @@
         <div class="m-xs-b-6">
 
             <div class="row">
+                <div class="card">
+                    <div class="card-section">
 
-                <h4>Here is a sample custom Blade Directive</h4>
+                        <h4>Here is a sample custom Blade Directive</h4>
 
-                @public
-                    <p>I can be seen by anyone</p>
-                @else
-                    <p>I can only be seen by Authenticated Users</p>
-                @endpublic
-
+                        @public
+                            <p>I can be seen by anyone</p>
+                        @else
+                            <p>I can only be seen by Authenticated Users</p>
+                        @endpublic
+                    </div>
+                </div>
             </div>
         </div>
     </div>
