3d0696b711bb8ad013525b3b3cfd593f4f35af8b
Brad Madigan <bradmadigan@gmail.com>
2017-08-22 09:46:09 -0400

    Move table into a card section

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -15,36 +15,45 @@
     <div class="container m-xs-b-4">
         <div class="m-xs-b-6">
 
-            <div class="row">
-                <table class="table mt3" cellpadding="0" cellspacing="0">
-                    <thead>
-                    <tr>
-                        <th class="pl2">Product</th>
-                        <th>Price</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($products as $product)
-                        <tr>
-                            <td class="ph2">
-                                <a href="{{ route('products.show', $product) }}" class="fw7">{{ $product->product_name }}</a>
-                                <p class="lh2 helper-text">Last Updated: <strong>{{ displayShortDate($product->updated_at) }}</strong></p>
-                            </td>
-                            <td>{{ formatAsCurrency($product->price) }}</td>
-                            <td>
-                                <a href="{{ route('products.edit', $product) }}"><i class="fa fa-pencil-square mr1" aria-hidden="true"></i></a>
+            <div class="alert alert-notify ">
+                * This table is a standard Eloquent with <strong>No</strong> pagination using just Blade.<br>
+                The Edit and Destroy are standard CRUD operations.
+            </div>
 
-                                {!! Form::open(['route' => ['products.destroy', $product->id], 'method' => 'DELETE', 'class' => 'inline']) !!}
-                                    <button type="submit" class="btn btn-link">
-                                        <i class="fa fa-trash" aria-hidden="true"></i>
-                                    </button>
-                                {!! Form::close() !!}
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+            <div class="row">
+                <div class="card">
+                    <div class="card-section">
+                        <table class="table mt3" cellpadding="0" cellspacing="0">
+                            <thead>
+                            <tr>
+                                <th class="pl2">Product</th>
+                                <th>Price</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($products as $product)
+                                <tr>
+                                    <td class="ph2">
+                                        <a href="{{ route('products.show', $product) }}" class="fw7">{{ $product->product_name }}</a>
+                                        <p class="lh2 helper-text">Last Updated: <strong>{{ displayShortDate($product->updated_at) }}</strong></p>
+                                    </td>
+                                    <td>{{ formatAsCurrency($product->price) }}</td>
+                                    <td>
+                                        <a href="{{ route('products.edit', $product) }}"><i class="fa fa-pencil-square mr1" aria-hidden="true"></i></a>
+
+                                        {!! Form::open(['route' => ['products.destroy', $product->id], 'method' => 'DELETE', 'class' => 'inline']) !!}
+                                            <button type="submit" class="btn btn-link">
+                                                <i class="fa fa-trash" aria-hidden="true"></i>
+                                            </button>
+                                        {!! Form::close() !!}
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
